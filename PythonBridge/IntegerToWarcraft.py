b40a133c13e776ae8d6a0477200bf737b97a8872
Eloi Strée <eloistree@gmail.com>
2024-10-23 21:07:17 +0200

    adjust index array and add anti afk for the teacher

--- PythonBridge/IntegerToWarcraft.py
+++ PythonBridge/IntegerToWarcraft.py
@@ -23,12 +23,12 @@ use_print_log=True
 player_index_to_window_index ={}
 
 ## use to broadcast on target window from source index id
-player_index_to_window_index [2]= [0]
-player_index_to_window_index [3]= [1]
-player_index_to_window_index [4]= [2]
-player_index_to_window_index [5]= [3]
-player_index_to_window_index [6]= [0,1,2,3]
-player_index_to_window_index [0]= [0,1,2,3,4,5,6,7,8,9,10,11]
+player_index_to_window_index [0]= [0,1,2,3]
+player_index_to_window_index [1]= [0]
+player_index_to_window_index [2]= [1]
+player_index_to_window_index [3]= [2]
+player_index_to_window_index [4]= [3]
+player_index_to_window_index [10]= [0,1,2,3,4,5,6,7,8,9,10,11]
 
 
 
